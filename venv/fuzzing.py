942b940fc93605710fe805b0f959b04f128f70bd
aliyilmaz020 <80054378+aliyilmaz020@users.noreply.github.com>
2023-03-10 11:06:07 -0500

    .

---
+++ venv/fuzzing.py
@@ -0,0 +1,17 @@
+import requests
+
+dosya = open("fuzzing.txt","r")
+icerik = dosya.read()
+dosya.close()
+header = {"Cookie": "_cfuvid=WDlonye8yEuGHnJq0Gx9KFJVeZsmh7mTZ8ZldW27rGE-1678462147045-0-604800000"}
+# print(icerik)
+for i in icerik.split("\n"):
+    print(i)
+    url="https://bandirma.edu.tr"+str(i)
+    sonuc=requests.get(url=url,headers=header)
+    if "200" in str(sonuc.status_code):
+        print("Dosya ya da dizin var",i)
+    else:
+        print("Dosya ya da dizin yok",i)
+
+# fuzzing: bir sunucuda ilgili dizinleri kontrol etmek için kullanılır.
